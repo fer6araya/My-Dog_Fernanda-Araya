376ac693165081cc664ea4469b994a2ec4c6474c
fer6araya <162595941+fer6araya@users.noreply.github.com>
2024-06-17 01:43:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function selectRandomWordFromArray(wordArray) {
+  const selector = Math.floor(Math.random() * wordArray.length);
+  return wordArray[selector];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const excuse = document.getElementById("excuse");
+  excuse.innerHTML =
+    selectRandomWordFromArray(who) +
+    " " +
+    selectRandomWordFromArray(action) +
+    " " +
+    selectRandomWordFromArray(what) +
+    " " +
+    selectRandomWordFromArray(when);
 };
